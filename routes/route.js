060e79b0adf9a1cd2a21b5d910984ffab4eab70f
07Akashh <aonecreations1@gmail.com>
2024-11-17 15:47:09 +0530

    add: api docs

--- routes/route.js
+++ routes/route.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const userRoute = require('./user.route.js');
 const carRoute = require('./car.route.js');
-const imageRoute = require('./image.route.js')
+const imageRoute = require('./image.route.js');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../postman/project.openapi.json');
 const app = express();
 
-// app.use(express.json());
-
+// Set up routes
 app.use('/user', userRoute);
 app.use('/car', carRoute);
 app.use('/images', imageRoute);
+
+// Serve Swagger docs
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Global error handler (optional)
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Something went wrong!' });
+});
 
 module.exports = app;
